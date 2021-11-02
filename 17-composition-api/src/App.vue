018df1e1aef542cd489733049da3fe13be3fff7a
nooobcoder <suryashi2013@gmail.com>
2021-11-02 09:27:12 +0530

    Composition API - Replace computed properties with computed function

--- 17-composition-api/src/App.vue
+++ 17-composition-api/src/App.vue
@@ -1,40 +1,47 @@
 <template>
   <section class="container">
-    <h2>{{ userName }}</h2>
+    <h2>{{ computed.getFullName }}</h2>
     <h3>{{ age }}</h3>
     <button @click.prevent="helpers.setNewAge">Change Age</button>
+    <div>
+      <input
+        type="text"
+        name="firstname"
+        id="firstname"
+        placeholder="First Name"
+        v-model.trim="firstName"
+      />
+      <input
+        type="text"
+        name="lastname"
+        id="lastname"
+        placeholder="Last Name"
+        v-model.trim="lastName"
+      />
+    </div>
   </section>
 </template>
 
 <script>
-import { reactive, toRefs } from "vue";
+import { ref, computed } from "vue";
 
 export default {
   setup() {
-    // https://v3.vuejs.org/guide/composition-api-setup.html#usage-with-templates
-    /* 
-      const userName = ref('nooobcoder')
-      const age = ref(30)
-    */
-    const user = reactive({ userName: "nooocoder", age: 30 });
+    const firstName = ref("");
+    const lastName = ref("");
 
-    const setNewAge = () => {
-      user.age += 10;
+    const getFullName = computed(() =>
+      firstName.value.length > 0 || lastName.value.length > 0
+        ? `${firstName.value} ${lastName.value}`
+        : undefined
+    );
+
+    return {
+      firstName,
+      lastName,
+      computed: { getFullName },
     };
-
-    const userRef = toRefs(user);
-    return { ...userRef, helpers: { setNewAge } };
   },
-  // data() {
-  //   return {
-  //     userName: "nooobcoder",
-  //   };
-  // },
-  // methods: {
-  //   setNewAge() {
-  //     this.age += 10;
-  //   },
-  // },
 };
 </script>
 
