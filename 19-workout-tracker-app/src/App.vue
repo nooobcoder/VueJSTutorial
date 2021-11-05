f721b383ca29fb7015d074036c0ba40725a76522
nooobcoder <suryashi2013@gmail.com>
2021-11-05 08:47:37 +0530

    Link app with VueX State

--- 19-workout-tracker-app/src/App.vue
+++ 19-workout-tracker-app/src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <div class="min-h-full font-Poppins box-border">
+  <div v-if="appReady" class="min-h-full font-Poppins box-border">
     <the-navigation />
+    <p>Render this</p>
     <router-view />
   </div>
 </template>
 
 <script>
 import TheNavigation from "./components/Navigation.vue";
+import { ref } from "vue";
+import { supabase } from "./supabase/init";
+import store from "./store/index";
 
 export default {
   components: { TheNavigation },
   setup() {
     // Create data / vars
+    const appReady = ref(null);
 
     // Check to see if user is already logged in
+    const user = supabase.auth.user();
 
     // If user does not exist, need to make app ready
+    if (!user) {
+      appReady.value = true;
+    }
 
     // Runs when there is a auth state change
     // if user is logged in, this will fire
-
-    return {};
+    supabase.auth.onAuthStateChange((_, session) => {
+      store.dispatch("setUser", session);
+      appReady.value = true;
+    });
+    return { appReady };
   },
 };
 </script>
--- 19-workout-tracker-app/src/store/index.js
+++ 19-workout-tracker-app/src/store/index.js
@@ -1,1 +1,23 @@
+import { Store } from "vuex";
 
+const store = new Store({
+  state: { user: null },
+  getters: {
+    getUser(state) {
+      return state.user;
+    },
+  },
+  mutations: {
+    setUser(state, payload) {
+      console.log(payload);
+      state.user = payload ? payload.user : null;
+    },
+  },
+  actions: {
+    setUser(ctx, payload) {
+      ctx.commit("setUser", payload);
+    },
+  },
+});
+
+export default store;
