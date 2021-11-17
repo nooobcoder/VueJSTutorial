f96630d45fbdc219c633433e40d4d1a38e032abc
nooobcoder <suryashi2013@gmail.com>
2021-11-17 20:42:05 +0530

    Hook project to firebase 🔌

---
+++ 20-invoice-app/src/firebase/firebaseinit.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC4GNDI2Zc-rpBsO7T3MohmAWPfhjnV59Q",
+  authDomain: "invoice-vue-app-dcc3d.firebaseapp.com",
+  projectId: "invoice-vue-app-dcc3d",
+  storageBucket: "invoice-vue-app-dcc3d.appspot.com",
+  messagingSenderId: "141905349185",
+  appId: "1:141905349185:web:8072493f4c4f0ef6d214c0",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp.firestore();
